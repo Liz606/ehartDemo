d559e0fd402886fa74d2c8478bd1ce40b22790c2
frend.kao <loncy.gao@31huiyi.com>
2017-07-26 11:34:06 +0800

    resolve ie doesn't support 'symbol' bug

--- config/polyfills.js
+++ config/polyfills.js
@@ -14,3 +14,8 @@ require('whatwg-fetch');
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
+
+// Support Symbol for IE
+if(typeof Symbol === 'undefined') {
+  window.Symbol = require('es6-symbol');
+}
