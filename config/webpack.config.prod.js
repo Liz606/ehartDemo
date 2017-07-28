ccfb990031f4280092afc47ed56c2e6ad440e370
frend.kao <loncy.gao@31huiyi.com>
2017-07-28 15:34:58 +0800

    load components just as needed

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -64,7 +64,7 @@ module.exports = {
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
     filename: 'static/js/[name].[chunkhash:8].js',
-    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath
   },
@@ -170,22 +170,12 @@ module.exports = {
       // enable scss with 'sass' loader and use 'style' loader as the same as css.
       {
         test: /\.scss$/, 
-        loader: ExtractTextPlugin.extract(
-          'style',
-          'css',
-          'sass',
-          extractTextPluginOptions
-        )
+        loader: 'style!css!sass'
       },
       // enable less with 'less' loader and user 'style | css' loader as the same as css.
       {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract(
-          'style',
-          'css',
-          'less',
-          extractTextPluginOptions
-        )
+        loader: 'style!css!less',
       }
     ]
   },
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import {Router, Route, IndexRoute} from 'react-router';
-
-import Layout from '../containers/layout';
-import DemoException from '../containers/demo-exception';
-import DemoMaterial from '../containers/demo-material';
-
 // 路由配置模块
-export default(
-  <Router>
-    <Route path="/" component={Layout}>
-      <IndexRoute component={DemoException} />
-
-      <Route path="/exception" component={DemoException} />
-      <Route path="/material" component={DemoMaterial} />
-    </Route>
-  </Router>
-);
+export default {
+  path: '/',
+  getComponent(nextState, callback) {
+    require.ensure([], require => {
+      callback(null, require('../containers/layout').default);
+    }, 'main');
+  },
+  indexRoute: {
+    getComponent(nextState, callback) {
+      require.ensure([], require => {
+        callback(null, require('../containers/demo-exception').default);
+      }, 'exception');
+    }
+  },
+  childRoutes: [{
+    path: 'exception',
+    getComponent(nextState, callback) {
+      require.ensure([], require => {
+        callback(null, require('../containers/demo-exception').default);
+      }, 'exception');
+    }
+  }, {
+    path: 'material',
+    getComponent(nextState, callback) {
+      require.ensure([], require => {
+        callback(null, require('../containers/demo-material').default);
+      }, 'material');
+    }
+  }]
+};
